781e4e4449d730883d7d1b5c95bf2b8c66acb706
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-01-09 13:13:46 +0200

    Added required stuff for heroku deployment maybe?

--- lib/frick_dmca_web/endpoint.ex
+++ lib/frick_dmca_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule FrickDmcaWeb.Endpoint do
   ]
 
   socket "/socket", FrickDmcaWeb.UserSocket,
-    websocket: true,
+    websocket: [45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
